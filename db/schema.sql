47b5055ec888cbc9fc2669ef82b69d785bf6fc31
RileyAlt <rileyaltenburg@gmail.com>
2023-03-08 19:38:40 -0600

    Seeds data

--- db/schema.sql
+++ db/schema.sql
@@ -18,7 +18,7 @@ CREATE TABLE roles (
 
 CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-    firt_name VARCHAR(30) NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30 NOT NULL,
     role_id INT,
     manager_id INT,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,20 @@
 INSERT INTO departments (name)
 VALUES ("Owner"),
-       ("Mortgage Broker"),
-       ("Processcor"),
-       ("Loan Officer Assistant"),
+       ("Sales"),
+       ("Processing"),
+       ("Assistant"),
        ("Realtor");
 
-INSERT INTO roles (id, title, salary, department_id)
-VALUES(001, )
+INSERT INTO roles (title, salary, department_id)
+VALUES ("CEO", "500,000", 1),
+       ("Loan Officer", "200,000", 2),
+       ("Processor", "100,000", 3),
+       ("Loan Officer Assistant", "75,000", 4),
+       ("Agent", "150,000", 5);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ("Nate", "Raich", 1, NULL),
+       ("Mike", "Arvold", 2, 1),
+       ("Cari", "Martin", 3, 1),
+       ("Riley", "Altenburg", 4, 2),
+       ("Tyler", "Miller", 5, NULL);
