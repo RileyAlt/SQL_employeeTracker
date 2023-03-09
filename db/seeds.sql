acfb492846083785588940b0ba66612dab415646
RileyAlt <rileyaltenburg@gmail.com>
2023-03-08 19:08:00 -0600

    schema start

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO departments (name)
+VALUES ("Owner"),
+       ("Mortgage Broker"),
+       ("Processcor"),
+       ("Loan Officer Assistant"),
+       ("Realtor");
+
+INSERT INTO roles (id, title, salary, department_id)
+VALUES(001, )
